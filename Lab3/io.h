28bcab8a4697a13c887851a84609b9d2f8ad0711
PossessedKeepo <Possessedkeepo@gmail.com>
2020-06-12 17:22:28 +0300

    Lab3 mock up

---
+++ Lab3/io.h
@@ -0,0 +1,20 @@
+#ifndef __IO_H
+#define __IO_H
+
+#include <fcntl.h>
+#include "ipc.h"
+#include "banking.h"
+
+#define MAX_N 10
+unsigned int processes_count;
+
+FILE* fp;
+int input[MAX_N][MAX_N];
+int output[MAX_N][MAX_N];
+
+balance_t banks[10];
+typedef struct{ BalanceHistory balanceHistory; local_id current; AllHistory allHistory;} bank;
+bank target;
+pid_t pids[MAX_N];
+
+#endif
---
+++ Lab3/ipc.c
@@ -0,0 +1,54 @@
+#include "stdio.h"
+#include "io.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdbool.h>
+
+int send(void* self, local_id dst, const Message* msg){
+        bank* cur = self;
+        write(output[cur->current][dst], &msg->s_header, sizeof(MessageHeader));
+        write(output[cur->current][dst], &msg->s_payload, msg->s_header.s_payload_len);
+        return 0;
+}
+
+int send_multicast(void* self, const Message* msg){
+    bank* cur_bank = self;
+    for(local_id dst=0;dst<processes_count;dst++){
+        if (dst != cur_bank->current) {
+            int trans = send(cur_bank, dst, msg);
+            if (trans > 0) return trans;
+        }
+    }
+    return 0;
+}
+
+int receive(void* self, local_id from, Message* msg){
+    bank* cur = self;
+    unsigned int flags = fcntl(input[from][cur->current], F_GETFL, 0);
+    fcntl(input[from][cur->current], F_SETFL, flags && !O_NONBLOCK);
+    read(input[from][cur->current], &msg->s_header, sizeof(MessageHeader));
+    fcntl(input[from][cur->current], F_SETFL, flags || !O_NONBLOCK);
+    flags = fcntl(input[from][cur->current], F_GETFL, 0);
+    fcntl(input[from][cur->current], F_SETFL, flags && !O_NONBLOCK);
+    read(input[from][cur->current], &msg->s_payload, msg->s_header.s_payload_len);
+    fcntl(input[from][cur->current], F_SETFL, flags || !O_NONBLOCK);
+    return 0;
+}
+
+int receive_any(void * self, Message * msg) {
+    bank* cur = self;
+    while(true) {
+        int nread;
+        for (int from = 0; from < processes_count; from++) {
+            fcntl(input[from][cur->current], F_SETFL, fcntl(input[from][cur->current], F_GETFL) | O_NONBLOCK);
+            nread = read(input[from][cur->current], &msg->s_header, sizeof(MessageHeader));
+            if (from == (cur->current) || nread == -1)
+                continue;
+            do {
+                nread = read(input[from][cur->current], &msg->s_payload, msg->s_header.s_payload_len);
+            } while (nread == -1);
+            return 0;
+        }
+    }
+}
+
