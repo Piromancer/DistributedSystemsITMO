70cd8999437aef1da178c03b9f4a85c3ca0314d2
PossessedKeepo <Possessedkeepo@gmail.com>
2020-06-12 19:12:17 +0300

    Lab3 first commit

--- Lab3/main.c
+++ Lab3/main.c
@@ -21,46 +21,80 @@ void child_start (bank* cur_bank, balance_t init_bal){
     for (timestamp_t timestamp = 0; timestamp < 256; timestamp++){
         cur_bank->balanceHistory.s_history[timestamp] = (BalanceState) { .s_balance = init_bal, .s_balance_pending_in = 0, .s_time = timestamp };
     }
+
+
+    cur_bank->lamp_time++;
+
     Message msg = {
             .s_header =
-                    { .s_magic = MESSAGE_MAGIC, .s_type = STARTED
+                    { .s_magic = MESSAGE_MAGIC, .s_type = STARTED, .s_local_time = get_lamport_time()
                     } };
-    timestamp_t timestamp = get_physical_time();
+
+    timestamp_t timestamp = get_lamport_time();
+
     msg.s_header.s_payload_len = strlen(msg.s_payload);
 
     printf((const char *) &msg, log_started_fmt, timestamp, cur_bank->current, getpid(), getppid(), cur_bank->balanceHistory.s_history[timestamp].s_balance);
     fprintf(fp, (const char *) &msg, log_started_fmt, timestamp, cur_bank->current, getpid(), getppid(), cur_bank->balanceHistory.s_history[timestamp].s_balance);
 
     send_multicast(&target, &msg);
+
+
+
     for (int i = 1; i <= processes_count-1; i++){
         Message message;
         if (i != cur_bank->current)
             receive(&target, i, &message);
+        if (cur_bank->lamp_time < message.s_header.s_local_time){
+            cur_bank->lamp_time = message.s_header.s_local_time;
+            cur_bank->lamp_time++;
+        }
     }
-    printf(log_received_all_started_fmt, get_physical_time(), cur_bank->current);
-    fprintf(fp, log_received_all_started_fmt, get_physical_time(), cur_bank->current);
+    printf(log_received_all_started_fmt, get_lamport_time(), cur_bank->current);
+    fprintf(fp, log_received_all_started_fmt, get_lamport_time(), cur_bank->current);
 
 
     size_t not_ready = processes_count - 2;
     bool flag = true;
     while (flag){
+
         receive_any(cur_bank, &msg);
+        if (cur_bank->lamp_time < msg.s_header.s_local_time){
+            cur_bank->lamp_time = msg.s_header.s_local_time;
+        }
+        cur_bank->lamp_time++;
 
         MessageType messageType = msg.s_header.s_type;
 
         if (messageType == TRANSFER){
             TransferOrder* transferOrder = (TransferOrder*) msg.s_payload;
-            timestamp_t transfer_time = get_physical_time();
+            timestamp_t transfer_time = msg.s_header.s_local_time;
 
             BalanceHistory* balanceHistory = &cur_bank->balanceHistory;
             balance_t res = 0;
 
             if (transferOrder->s_src == cur_bank->current){
-                res = -transferOrder->s_amount;
+                if (cur_bank->lamp_time < transfer_time){
+                    cur_bank->lamp_time = transfer_time;
+                }
+                cur_bank->lamp_time++;
+                timestamp_t sendTime = get_lamport_time();
+                for (timestamp_t i = sendTime; i < 256; i++){
+                    balanceHistory->s_history[i].s_balance -= transferOrder->s_amount;
+                }
+
+                msg.s_header.s_local_time = sendTime;
+
+                //res = -transferOrder->s_amount;
                 send(&target, transferOrder->s_dst, &msg);
-                printf(log_transfer_out_fmt, get_physical_time(), cur_bank->current, transferOrder->s_amount, transferOrder->s_dst);
-                fprintf(fp, log_transfer_out_fmt, get_physical_time(), cur_bank->current, transferOrder->s_amount, transferOrder->s_dst);
+                printf(log_transfer_out_fmt, sendTime, cur_bank->current, transferOrder->s_amount, transferOrder->s_dst);
+                fprintf(fp, log_transfer_out_fmt, sendTime, cur_bank->current, transferOrder->s_amount, transferOrder->s_dst);
             } else if (transferOrder->s_dst == cur_bank->current){
+
+                //todo
+                if (cur_bank->lamp_time){};
+
+
                 res = +transferOrder->s_amount;
                 Message ack;
                 ack.s_header = (MessageHeader) {
